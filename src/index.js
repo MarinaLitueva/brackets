b427a2864ecdbee5bdb8b5b994a7cb9c7a601977
Marina <lituevam@gmail.com>
2020-03-10 22:30:51 +0300

    feat: add solution brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+  
+    for (let i = 0; i < str.length; i++) {
+      for (let j = 0; j < bracketsConfig.length; j++) {
+        if (str[i] === bracketsConfig[j][1]) {
+          if (stack[stack.length - 1] === bracketsConfig[j][0]) {
+            stack.pop();
+          } else {
+            stack.push(str[i]);
+          }
+        }else if (str[i] === bracketsConfig[j][0]) {
+          stack.push(str[i]);
+        }
+      }
+    }
+    if (stack.length === 0) {
+      return true;
+    }
+    return false;
 }
